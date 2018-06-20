ba6eba25de08730b46e8c66f71ceb9c40ca82456
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-20 16:56:12 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,13 @@
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :songs, through: :artists
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Genre.all.find{|genre| genre.slug == slug}
+  end
+
 end
